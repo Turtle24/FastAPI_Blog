20c77115ea2dc856bd38a3b6d49854a2b45664a2
Turtle24 <aidan.nell@yahoo.com>
2021-03-05 21:29:51 +0100

    fatsapi exploration

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data': {'name': 'Aidan'}}
+@app.get('/blog')
+def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from db'}
+    else:
+        return {'data': f'{limit} blogs from db'}
 
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+@app.get('/blog/{blog_id}')
+def show(blog_id: int):
+    return {'data': blog_id}
+
+@app.get('/blog/{blog_id}/comments')
+def comments(blog_id: int, limit: int = 10):
+    return {'data': {1,2}}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': f'Blog is created with {blog.title}'}
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port= 9000)
